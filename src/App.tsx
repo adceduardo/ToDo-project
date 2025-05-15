79ef7d7c8c044b1408d8bfbb2041c40e677e27b4
EDUARDO ALVES DE CARVALHO <105119276+adceduardo@users.noreply.github.com>
2025-05-14 21:26:51 -0300

    [site] correção do bug que deixava criar uma tarefa vazia

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import { Header } from './components/Header/Header'
-import { Content } from './components/Content/Content'
+import { Header } from './components/Header/Header';
+import { Content } from './components/Content/Content';
 
-import './global.css'
+import './global.css';
 import { useState } from 'react';
 
 export function App() {
-  const [ tasks, setTasks ] = useState([]);
-  
-  function createNewTask(task){
-    setTasks([...tasks, 
-      task
-    ])
+  const [tasks, setTasks] = useState([]);
+
+  function createNewTask(task) {
+    if (task.title != '') {
+      setTasks([...tasks, task]);
+    }
   }
 
   function completeTask(index) {
-    var aux = [...tasks]; 
-    aux[index] = { ...aux[index], isComplete: !aux[index].isComplete}; 
-    setTasks(aux); 
+    var aux = [...tasks];
+    aux[index] = { ...aux[index], isComplete: !aux[index].isComplete };
+    setTasks(aux);
   }
 
-  
-  function deleteTask( index ){
-    const aux = tasks.filter((task, i) => i != index)
-    setTasks(aux); 
+  function deleteTask(index) {
+    const aux = tasks.filter((task, i) => i != index);
+    setTasks(aux);
   }
-
 
   return (
     <>
-      <Header tasks={tasks} createNewTask={createNewTask}/>
-      <Content tasks={tasks} completeTask={completeTask} deleteTask={deleteTask}/>
+      <Header tasks={tasks} createNewTask={createNewTask} />
+      <Content
+        tasks={tasks}
+        completeTask={completeTask}
+        deleteTask={deleteTask}
+      />
     </>
-  )
+  );
 }
-
-
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,39 +1,36 @@
-import styles from "./Header.module.css"
-import logo from '../../assets/logo.png'
+import styles from './Header.module.css';
+import logo from '../../assets/logo.png';
 
-import { AiOutlinePlusCircle } from 'react-icons/ai'
-import { useRef } from "react"
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useRef } from 'react';
 
-export function Header({ tasks, createNewTask }){
-    const inputRef = useRef(null)
-    
-    function handleSumbit(){
-        event.preventDefault()
-        createNewTask(
-            {
-                
-                id: tasks.length+1,
-                title: inputRef.current.value,
-                isComplete: false
-            }                   
-            );
-        inputRef.current.value = "";
-    }
+export function Header({ tasks, createNewTask }) {
+  const inputRef = useRef(null);
 
-    return(
-        <div>
-            <header className={styles.header}>
-                <img src={logo}/>
+  function handleSumbit() {
+    event.preventDefault();
 
-                
-            <form onSubmit={handleSumbit} className={styles.formTask}>
-                    <input ref={inputRef} placeholder="Adicione uma nova tarefa"/>
-                    <button type="submit" className={styles.create}>
-                        Criar
-                        <AiOutlinePlusCircle size={20}/>
-                    </button>
-            </form>
-            </header>
-        </div>
-    )    
+    createNewTask({
+      id: tasks.length + 1,
+      title: inputRef.current.value,
+      isComplete: false,
+    });
+    inputRef.current.value = '';
+  }
+
+  return (
+    <div>
+      <header className={styles.header}>
+        <img src={logo} />
+
+        <form onSubmit={handleSumbit} className={styles.formTask}>
+          <input ref={inputRef} placeholder="Adicione uma nova tarefa" />
+          <button type="submit" className={styles.create}>
+            Criar
+            <AiOutlinePlusCircle size={20} />
+          </button>
+        </form>
+      </header>
+    </div>
+  );
 }
